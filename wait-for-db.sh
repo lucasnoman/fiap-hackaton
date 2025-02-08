052170574889af663aeb54804405f8d4711b31c9
Vinicius Gomes <vinigomes.0311@gmail.com>
2025-02-08 02:34:31 -0300

    chore: comment out CodeBuild trigger and database wait logic in deployment scripts

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo "Waiting for database to be ready..."
-until nc -z -v -w30 $(echo "$DATABASE_URL" | sed -E 's/.*@([^:]+):.*/\1/') $(echo "$DATABASE_URL" | sed -E 's/.*:([0-9]+).*/\1/')
-do
-  echo "Waiting for database connection..."
-  sleep 5
-done
+# until nc -z -v -w30 $(echo "$DATABASE_URL" | sed -E 's/.*@([^:]+):.*/\1/') $(echo "$DATABASE_URL" | sed -E 's/.*:([0-9]+).*/\1/')
+# do
+#   echo "Waiting for database connection..."
+#   sleep 5
+# done
 
 echo "Database is ready! Running migrations..."
 npx prisma migrate deploy
